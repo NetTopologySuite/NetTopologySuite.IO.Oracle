8bda105f1ab4b2b80ecf84fdd32729ccfab7ac4e
Mathijs Dumon <mathijs.dumon@merkator.com>
2022-01-24 14:17:23 +0100

    Adding first steps for Oracle DB container integration tests

---
+++ test/NetTopologySuite.IO.Oracle.Test/IntegrationTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using NetTopologySuite.IO;
+
+namespace NetTopologySuite.IO.Oracle.Connection.Test
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class IntegrationTest
+    {
+        private const string testTableName = "NTS_TEST_GEO_DATA";
+
+        /// <summary>
+        /// Connect to the database
+        /// </summary>
+        [Test]
+        public void TestConnection()
+        {
+            OracleHelper.OpenConnection();
+        }
+
+        [Test]
+        public void TestCreateGeometryTable()
+        {
+
+            using var connection = OracleHelper.OpenConnection();
+
+            string res = OracleHelper.CreateGeometryTable(connection, testTableName);
+            OracleHelper.DropGeometryTable(connection, testTableName);
+
+            // TODO this is pretty dumb, need to check exact output
+            Assert.IsTrue(!string.IsNullOrWhiteSpace(res));
+        }
+
+        [Test]
+        // Assumption GEO_DATA table exists.
+        // Write a new created Geometry object to the database.
+        public void WriteGeometryTable()
+        {
+            // Read WKT into geometry object.
+            var wr = new WKTReader { IsOldNtsCoordinateSyntaxAllowed = false };
+            var correctCCW = "POLYGON((10 10, 20 10, 20 20, 10 20, 10 10),(5 5,5 6,6 6,6 5,5 5))";
+            var geom = wr.Read(correctCCW);
+            Console.WriteLine("Geometry: {0}", geom);
+
+            // Write geometry object into UDT object.
+            var oracleWriter = new OracleGeometryWriter();
+            var udt = oracleWriter.Write(geom);
+
+            // Open connection
+            using var connection = OracleHelper.OpenConnection();
+
+            // Drop & Create Geometry table.
+            OracleHelper.CreateGeometryTable(connection, testTableName);
+
+            var queryString = $"INSERT INTO {testTableName} (data) VALUES (:geo)";
+
+            using OracleCommand command = new OracleCommand(queryString, connection);
+            var geometryParam = new OracleParameter()
+            {
+                ParameterName= "geo",
+                DbType = DbType.Object,
+                Value = udt,
+                Direction = ParameterDirection.Input,
+                UdtTypeName = "MDSYS.SDO_GEOMETRY"
+            };
+            command.Parameters.Add(geometryParam);
+            command.ExecuteNonQuery();
+
+            // Clean up: drop table again
+            OracleHelper.DropGeometryTable(connection, testTableName);
+        }
+
+
+    }
+
+
+
+}
---
+++ test/NetTopologySuite.IO.Oracle.Test/OracleHelper.cs
@@ -0,0 +1,59 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace NetTopologySuite.IO.Oracle.Connection.Test
+{
+    public static class OracleHelper
+    {
+        /// <summary>
+        /// Opens a connection to the test database
+        /// </summary>
+        /// <returns></returns>
+        public static OracleConnection OpenConnection()
+        {
+            var conStringUser = ConfigurationManager.AppSettings["TestDBConnectionString"];
+            OracleConnection con = new OracleConnection(conStringUser);
+            con.Open();
+            return con;
+        }
+
+        /// <summary>
+        /// Drops (if it exists) and recreates the given table.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="tableName"> </param>
+        /// <returns>The name of the table as returned from sys.all_tables</returns>
+        public static string CreateGeometryTable(OracleConnection connection, string tableName)
+        {
+            OracleHelper.DropGeometryTable(connection, tableName);
+
+            var queryString = $"CREATE TABLE {tableName} (data MSYS.SDO_GEOMETRY)";
+            using OracleCommand command = new OracleCommand(queryString, connection);
+            command.ExecuteNonQuery();
+
+            var queryString2 = $"SELECT TABLE_NAME FROM sys.all_tables WHERE TABLE_NAME = '{tableName}'";
+            using OracleCommand command2 = new OracleCommand(queryString2, connection);
+            return (string)command2.ExecuteScalar();
+        }
+
+        /// <summary>
+        /// Drops (if it exists) the given table.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="tableName"></param>
+        public static void DropGeometryTable(OracleConnection connection, string tableName)
+        {
+            // Remove 'GEO_DATA' table
+            var queryString = $@"BEGIN
+                EXECUTE IMMEDIATE 'DROP TABLE {tableName}';
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        IF SQLCODE != -942 THEN
+                            RAISE;
+                        END IF;
+                END;";
+            using OracleCommand command = new OracleCommand(queryString, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
